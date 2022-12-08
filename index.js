e5d40456cf8d53e4337072db305780b71097a214
Nishal Barman <32856353+nishalbarman@users.noreply.github.com>
2022-12-08 23:48:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const accountSid = "AC4f321c495eaecb2c31bf1df060d6dced";
-const authToken = "43f53676d0857ddf58bc686657567f97";
+const authToken = "41700b3720dfbac8db1a8b33facc9264";
 const client = require("twilio")(accountSid, authToken);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7000;
 
 app.get("/", (req, res) => {
   res.send("Nope brother here you can not find anything... HaHahaha");
